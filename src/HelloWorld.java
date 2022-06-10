0c9ffc97a2daab6a4d2ed4d7e41169f6717a27f7
orgeron.kevin <orgeron.kevin@gmail.com>
2022-06-09 22:14:50 -0500

    exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,42 @@
 public class HelloWorld {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+//TODO: CODE GOES HERE
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 0;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "ThIs Is mY sIlLy StRiNg";
+        System.out.println(myString);
+
+
+        myString = "a";
+        //        myString = 3.14159;
+
+//        long myNumber = ;
+
+        long myNumber = (long) 3.14;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+
+//        String class =
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+
+
+
+
     }
-
-
-
 }
