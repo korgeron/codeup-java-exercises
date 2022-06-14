599f2a78d3ad2bd587f120d6f740a0186c982b5c
orgeron.kevin <orgeron.kevin@gmail.com>
2022-06-13 23:03:12 -0500

    exercise catchup

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
@@ -26,9 +28,79 @@ public class ControlFlowExercises {
 //        int num = 2;
 //        do {
 //            System.out.println(num);
-//            num *= 2;
+////            num *= 2;
+//            num = (int) Math.pow(num, 2);
+//        } while (num <= 1000000);
+
+//        for (int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
 //
-//        }while (num <= 1000000);
+//        for(long i = 2; i <= 10000000; i*=i) {
+//            System.out.println(i);
+//        }
+
+
+        //TODO: FIZZBUZZ
+//        for (int c = 1; c <= 100; c++) {
+//            if (c % 15 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (c % 5 == 0){
+//                System.out.println("Buzz");
+//            } else if (c % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(c);
+//            }
+//        }
+
+
+        Scanner scanner = new Scanner(System.in);
+
+//TODO: TABLE
+
+//        while (true) {
+//            System.out.println("Please enter a number.");
+//            int userNum = scanner.nextInt();
+//            String title = "Here is your table!\n\nnumber | squared | cubed\n------ | ------- | -----";
+//            System.out.println(title);
+//            for (long table = 1; table <= userNum; table++) {
+//                long squared = table * table;
+//                long cubed = table * table * table;
+//                System.out.printf("%-6d | %-7d | %-5d %n", table, squared, cubed);
+//            }
+//            System.out.println("Do you want to enter another number? (Y/N)");
+//            String userChoice = scanner.next();
+//            if (userChoice.equalsIgnoreCase("n")) {
+//                break;
+//            }
+//        }
+
+
+//TODO: GRADES
+
+//        while (true) {
+//            System.out.println("Enter students grade: ");
+//            int studentGrade = scanner.nextInt();
+//            if (studentGrade > 0 && studentGrade <= 59) {
+//                System.out.println('F');
+//            } else if (studentGrade > 59 && studentGrade <= 66) {
+//                System.out.println('D');
+//            } else if (studentGrade > 66 && studentGrade <= 79) {
+//                System.out.println('C');
+//            } else if (studentGrade > 79 && studentGrade <= 87) {
+//                System.out.println('B');
+//            } else if (studentGrade > 87 && studentGrade <= 100) {
+//                System.out.println('A');
+//            }
+//            System.out.println("Do you need to enter another grade? (Y/N)");
+//            String anotherGrade = scanner.next();
+//            if(anotherGrade.equalsIgnoreCase("n")) {
+//                break;
+//            }
+//        }
+//    }
+//}
 
 
     }
---
+++ src/MethodsExercises.java
@@ -0,0 +1,26 @@
+public class MethodsExercises {
+
+    public static int addition(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int subtraction(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public static int multiplication(int num1, int num2){
+        return num1 * num2;
+    }
+
+    public static int division (int num1, int num2){
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(addition(1, 2));
+//        System.out.println(subtraction(3, 1));
+//        System.out.println(multiplication(3, 1));
+//        System.out.println(division(10, 2));
+
+    }
+}
